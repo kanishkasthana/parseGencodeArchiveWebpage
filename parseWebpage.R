57387407534d4d13d71ed7baced792ce3ca0304d
kanishk <kani.asthana@gmail.com>
2015-12-23 02:22:09 -0800

    Changing code again look at diff

--- parseWebpage.R
+++ parseWebpage.R
@@ -1,16 +1,22 @@
-#Script Created by Kanishk Asthana
+#Script Created by Kanishk Asthana to get statistics for previous gencode releases.
 archivePage=readLines("http://www.gencodegenes.org/stats/archive.html")
 #Finding gencode version indices
 versionIndices=grep("Version\\s[0-9]",archivePage)
 versionLine=archivePage[versionIndices]
 
-versionReleasePeriod=sapply(versionLine, function(line){
+#Storing Names of all Versions in a vector
+versionReleasePeriod=as.character(sapply(versionLine, function(line){
   #Deleting First part of the String
   line=sub(".*\\(","",line)
   #Deleting Second unwanted part of String
   line=sub("\\sfreeze,\\s.*\\).*","",line)
   print(line)
   return(line)
-})
-#Storing Names of all Version in a vector
-versionReleasePeriod=as.character(versionReleasePeriod)
+}))
+
+#Storing Version Numbers in a vector
+versions=as.character(sapply(versionLine, function(line){
+  match=regexpr("Version\\s.{1,2}",line)
+  versionText=as.character(regmatches(line,match))
+  return(versionText)
+}))
